15acf27ce2029f531fd6afe4a8c45f41bdc3883c
david zambrano corral <Yayoamigo>
2023-02-27 21:31:46 -0500

    Add messenger

--- API/index.js
+++ API/index.js
@@ -1,6 +1,7 @@
 // Import necessary libraries
 const express = require("express"); // Framework to create a REST API
 const app = express(); // Create an instance of the express application
+const http = require("http"); // Library for creating an HTTP server
 const mongoose = require("mongoose"); // Library for connecting to MongoDB
 const helmet = require("helmet"); // Library for setting HTTP headers to improve security
 const morgan = require("morgan"); // Library for logging HTTP requests
@@ -10,7 +11,9 @@ const authRouter = require("./routes/auth"); // Authentication routes
 const postRoute = require("./routes/posts"); // Post routes
 const path = require('path'); // Library for working with file and directory paths
 const multer = require("multer"); // Library for handling file uploads
-const fs = require('fs'); // Library for working with the file system
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
+const io = require('socket.io');
 
 // Load environment variables from a .env file
 dotenv.config();
@@ -18,63 +21,79 @@ dotenv.config();
 // Connect to MongoDB using the URL stored in the environment variable
 mongoose.connect(
     process.env.MONGO_URL, 
-    {useNewUrlParser: true, useUnifiedTopology: true},
+    {useNewUrlParser: true,useUnifiedTopology: true},
     ()=>{
-        console.log("connected to Mongo")
+    console.log("connected to Mongo")
     }
-);
-
-// Set up middleware to parse JSON data and log HTTP requests
-app.use(express.json());
-app.use(morgan("common"));
-
-// Set up middleware to set HTTP headers for improved security
-app.use(helmet({
-  contentSecurityPolicy: {
+    );
+    
+    // Create an HTTP server using the express app
+    const server = http.createServer(app);
+    
+    const socketServer = io(server);
+  
+    
+    // Set up middleware to parse JSON data and log HTTP requests
+    app.use(express.json());
+    app.use(morgan("common"));
+    
+    // Set up middleware to set HTTP headers for improved security
+    app.use(
+    helmet({
+    contentSecurityPolicy: {
     directives: {
-      ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-      "img-src": ["'self'", "data:", "blob:"]
-    }
-  }
-}));
-
-// Set up routes for users, authentication, and posts
-app.use("/api/users", userRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/posts", postRoute);
-
-// Set up multer middleware for handling file uploads and set the file storage location and name
-const storage = multer.diskStorage({
+    ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+    "img-src": ["'self'", "data:", "blob:"],
+    "connect-src": ["'self'", "http://localhost:8900", "ws://localhost:8900"],
+    },
+    },
+    })
+    );
+    
+    // Set up routes for users, authentication, and posts
+    app.use("/api/users", userRouter);
+    app.use("/api/auth", authRouter);
+    app.use("/api/posts", postRoute);
+    app.use("/api/conversations", conversationRoute);
+    app.use("/api/messages", messageRoute);
+    
+    // Set up multer middleware for handling file uploads and set the file storage location and name
+    const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "images/assets");
+    cb(null, "images/assets");
     },
     filename: (req, file, cb) => {
-      cb(null, req.body.name);
+    cb(null, req.body.name);
     },
-});
-
-const upload = multer({ storage: storage });
-
-// Set up route to handle file uploads and return the filename
-app.post("/api/upload", upload.single("file"), (req, res) => {
+    });
+    
+    const upload = multer({ storage: storage });
+    
+    // Set up route to handle file uploads and return the filename
+    app.post("/api/upload", upload.single("file"), (req, res) => {
     try {
-      const fileName = req.file.filename;
-      return res.status(200).json({ fileName });
+    const fileName = req.file.filename;
+    return res.status(200).json({ fileName });
     } catch (error) {
-      console.error(error);
+    console.error(error);
     }
-});
-
-// Serve static files (e.g. CSS, JS, images) from the 'public' directory and the uploaded images from the 'images/assets' directory
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images', express.static(path.join(__dirname, 'images/assets')));
-
-// Set up a catch-all route that sends the index.html file for client-side routing
-app.get('/*', (req,res)=>{
+    });
+    
+    // Serve static files (e.g. CSS, JS, images) from the 'public' directory and the uploaded images from the 'images/assets' directory
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use('/images', express.static(path.join(__dirname, 'images/assets')));
+    
+    // Set up a catch-all route that sends the index.html file for client-side routing
+    app.get('/*', (req,res)=>{
     res.sendFile(path.join(__dirname, '..', 'API', 'public', 'index.html'))
-});
+    });
 
-// Start the server on port 8000 and log a message to the console
-app.listen(8000, ()=>{
-    console.log('server running');
-});
+    const sockets = require('./socket');
+    
+    // Start the HTTP server on port 8000 and log a message to the console
+    const PORT = process.env.PORT || 8000;
+    server.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+    });
+
+    sockets.listen(socketServer);
--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import Messenger from "./pages/messenger/Messenger";
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { useSelector } from "react-redux";
 
@@ -13,6 +14,8 @@ function App() {
         <Route path="/" element={user? <Home /> : <Register/>} />
         <Route path="/register" element={user ? <Navigate to="/"/> : <Register />} />
         <Route path="/login" element={ user ? <Navigate to="/"/> : <Login />} />
+        <Route path="/messenger"
+          element={ <Messenger />} />
         <Route path="/profile/:username" element={<Profile />} />
       </Routes>
     </Router>
